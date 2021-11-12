f4e72f09e79dde54f5960052d183479123eadca1
Jakub Narębski <jnareb@mat.umk.pl>
2021-11-12 11:34:55 +0100

    generate_and_compile.sh: Add comments and some error handling

--- benchmark/generate_and_compile.sh
+++ benchmark/generate_and_compile.sh
@@ -1,13 +1,28 @@
 #!/bin/bash
 
+
+# arguments handling
+if [ -z "$1" ]; then
+  echo "Usage: $0 JSON_FILE"
+  exit 1
+fi
+if [ ! -r "$1" ]; then
+  echo "File '$1' does not exist or is not readable"
+  exit 2
+fi
+
+# generate sample in a format understood by json2cuda.py script
 TMP_FNAME=`mktemp`
+head -1 "$1" >"$TMP_FNAME"
 
-head -1 $1 > $TMP_FNAME
+# print some debugging information
+cat "$TMP_FNAME"
 
-cat $TMP_FNAME
+# generate parser configuration
+python3 ~/GPU-IDUB/json2meta/poc/json2cuda.py "$TMP_FNAME" >~/GPU-IDUB/meta-json-parser/benchmark/data_def.cuh
 
-python3 ~/GPU-IDUB/json2meta/poc/json2cuda.py $TMP_FNAME > ~/GPU-IDUB/meta-json-parser/benchmark/data_def.cuh
-
+# compile
 make -C ~/GPU-IDUB/meta-json-parser/build/ meta-json-parser-benchmark
 
-rm $TMP_FNAME
+# cleanup
+rm "$TMP_FNAME"
