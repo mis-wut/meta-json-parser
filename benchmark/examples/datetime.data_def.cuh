f104bea4230f179ff635b8787f3393560d28f171
Jakub Narębski <jnareb@mat.umk.pl>
2022-03-22 14:15:45 +0100

    benchmark/examples: Add simple datetime.* tests

---
+++ benchmark/examples/datetime.data_def.cuh
@@ -0,0 +1,21 @@
+// INCLUDES
+#include <meta_json_parser/action/datetime/jdatetime.cuh>
+#include <meta_json_parser/meta_utility/metastring.h>
+
+// EXAMPLE:
+// { "date": "2021-03-18 00:16:48" }
+
+// KEYS
+using K_L1_date = mp_string<'d', 'a', 't', 'e'>;
+
+// FORMATS
+using DatetimeFormat_YYYYMMDD_HHMMSS = metastring("%Y-%m-%d %H:%M:%S");
+
+// DICT
+#define STATIC_STRING_SIZE 32
+template<template<class, int> class StringFun, class DictOpts>
+using DictCreator = JDict < mp_list <
+    mp_list<K_L1_date, JDatetime<DatetimeFormat_YYYYMMDD_HHMMSS, K_L1_date>>,
+>,
+    DictOpts
+> ;
