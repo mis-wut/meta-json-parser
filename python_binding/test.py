e63e7e0279de3b537d8d0f7981e97ae879ff8c76
Piotr Przymus <piotr.przymus@gmail.com>
2022-04-05 20:30:48 +0200

    benchmark init

--- python_binding/test.py
+++ python_binding/test.py
@@ -1,4 +1,34 @@
-import cudf 
+import cudf
 import test
+import timeit
+import sys
 
-test.wrapped_test("../benchmark/examples/warszawa.jsonl", 573)
+if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f"Usage {sys.argv[0]} fname repeats (cudf|meta)")
+
+    fname = sys.argv[1]
+    time_meta = 0
+    time_cudf = 0
+    N = int(sys.argv[2])
+
+    with open(fname) as f:
+       lines = len(f.readlines())
+
+    for i in range(N):
+       if "meta" in sys.argv[3]:
+            starttime = timeit.default_timer()
+            df = test.wrapped_test(fname, lines)
+            time_meta = timeit.default_timer() - starttime
+
+       if "cudf" in sys.argv[3]:
+            starttime = timeit.default_timer()
+            df1 = cudf.io.json.read_json(fname, lines =True, orient='columns')
+            time_cudf = timeit.default_timer() - starttime
+
+    if "meta" in sys.argv[3]:
+       print(f"META runs {N} total time {time_meta}")
+
+    if "cudf" in sys.argv[3]:
+       print(f"CUDF runs {N} total time {time_cudf}")
+
