eb72c696b2838c56965a97c28b6cd1a50bdc7e4a
Jakub Narębski <jnareb@gmail.com>
2022-05-25 21:50:00 +0200

    run_benchmark_all_types.sh: run_benchmarks_str string <N>

--- benchmark/run_benchmarks_all_types.sh
+++ benchmark/run_benchmarks_all_types.sh
@@ -39,8 +39,16 @@ run_generate () {
 }
 
 run_benchmarks () {
+	# TODO: make it generic, add "1a" suffix only if it does not exists
 	typename="${1}1a"
+	#typename="${1}"
 	size=1000000
+
+	#if [[ -n "$2" && -f "${2}" ]]; then
+	#	data_def_file="${2}"
+	#else
+	#	data_def_file="$EXAMPLES_DIR/${typename}.data_def.cuh"
+	#fi
 
 	# DEBUG
 	#echo "typename=$typename; size=$size"
@@ -62,6 +70,30 @@ run_benchmarks () {
 		--output-csv="$NOTEBOOKS_DIR/benchmark_libcudf_cudf=21.10-docker_${typename}_dtypes.csv"
 }
 
+run_benchmarks_str () {
+	typename="${1}1a"
+	max_size=$2
+
+		if [ ! -f "$EXAMPLES_DIR/${typename}_10.jsonl" ]; then
+		head -10 \
+			 "$EXAMPLES_DIR/${typename}_${size}.jsonl" \
+			>"$EXAMPLES_DIR/${typename}_10.jsonl"
+	fi
+
+	"$RUN_CHECK" "$EXAMPLES_DIR/${typename}_${max_size}.data_def.cuh" "$EXAMPLES_DIR/${typename}_10.jsonl" || exit 1
+
+	benchmark "${typename}" \
+		--output-csv="$NOTEBOOKS_DIR/benchmark_metaparser-co=1_cudf=21.10-docker_${typename}-static_${max_size}.csv"
+	benchmark "${typename}" \
+		--version=1 --max-string-size=${max_size} \
+		--output-csv="$NOTEBOOKS_DIR/benchmark_metaparser-co=1_cudf=21.10-docker_${typename}-version=1_${max_size}.csv"
+	benchmark "${typename}" \
+		--version=2 --max-string-size=${max_size} \
+		--output-csv="$NOTEBOOKS_DIR/benchmark_metaparser-co=1_cudf=21.10-docker_${typename}-version=2_${max_size}.csv"
+	benchmark "${typename}" \
+		--version=3 --max-string-size=${max_size} \
+		--output-csv="$NOTEBOOKS_DIR/benchmark_metaparser-co=1_cudf=21.10-docker_${typename}-version=3_${max_size}.csv"
+}
 
 run_generate_all () {
 	size=1000000
@@ -89,13 +121,21 @@ run_benchmarks_all () {
 # NOTE: needs meta-json-parser-benchmark
 #run_benchmarks_all
 #run_benchmarks datetime
-run_benchmarks datetime_as_string
+#run_benchmarks datetime_as_string
 #run_benchmarks bool
 #run_benchmarks string
 #run_benchmarks nullable_string
 #run_benchmarks integer
 #run_benchmarks fixed
 #run_benchmarks float
+run_benchmarks_str string 16
+#run_benchmarks_str string 32
+#run_benchmarks_str string 64
+#run_benchmarks_str string 128
+#run_benchmarks_str string 256
+#run_benchmarks_str string 512
+#run_benchmarks_str string 1024
+#run_benchmarks_str string 2048
 
 #"$RUN_CHECK" "$EXAMPLES_DIR/bool.data_def.cuh" "$EXAMPLES_DIR/bool_10.json"
 #"$RUN_BENCHMARKS" --workspace-size=32 --const-order=1 --samples=50 \
